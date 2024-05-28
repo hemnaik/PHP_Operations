d5ac8ce90fa77dcf03185897c89df13f5dade34e
HemKumar Naik <142394510+hemnaik@users.noreply.github.com>
2024-05-28 12:23:28 +0530

    To rename the existing name of any table

---
+++ customer_renametable.php
@@ -0,0 +1,27 @@
+<body>
+<?php
+     $con=mysqli_connect("localhost","root","");
+	 mysqli_select_db($con,"customer");
+	 
+	 if($con)
+	 {
+                echo "CONNECTION ESTABLISHED"."</br>";
+
+	         $delete_table="RENAME TABLE customer_new TO customer_table";
+		 $result=mysqli_query($con,$delete_table);
+		 if($result==true)
+		 {
+		      echo "TABLE RENAMED"."</br>";
+		 }
+		 else
+		 {
+		      echo "TABLE NOT RENAMED"."</br>";
+		 }
+	 }
+         else
+         {
+               echo "CONNECTION FAILED"."</br>";
+         }
+  ?>
+</body>
+</html>
